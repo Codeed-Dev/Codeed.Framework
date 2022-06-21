3f170eba91a26d9d09aa4ce49dd1e7383afe16ad
Guilherme Gavazzoni <guilherme.gavazzoni@outlook.com>
2022-06-21 20:26:41 -0300

    changed HttpService to verify if user is authorized

--- net/src/Codeed.Framework.Services.Http/HttpService.cs
+++ net/src/Codeed.Framework.Services.Http/HttpService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Codeed.Framework.AspNet;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,12 +9,12 @@ namespace Codeed.Framework.Services
     {
         public static class WithParameters<T1>
         {
-            public abstract class WithResponse<TRet> : ControllerBase, IService<T1, TRet>
+            public abstract class WithResponse<TRet> : BaseController, IService<T1, TRet>
             {
                 public abstract Task<TRet> ExecuteAsync(T1 param1, CancellationToken cancellationToken);
             }
 
-            public abstract class WithoutResponse : ControllerBase, IServiceWithoutResponse<T1>
+            public abstract class WithoutResponse : BaseController, IServiceWithoutResponse<T1>
             {
                 public abstract Task ExecuteAsync(T1 param1, CancellationToken cancellationToken);
             }
@@ -21,12 +22,12 @@ namespace Codeed.Framework.Services
 
         public static class WithParameters<T1, T2>
         {
-            public abstract class WithResponse<TRet> : ControllerBase, IService<T1, T2, TRet>
+            public abstract class WithResponse<TRet> : BaseController, IService<T1, T2, TRet>
             {
                 public abstract Task<TRet> ExecuteAsync(T1 param1, T2 param2, CancellationToken cancellationToken);
             }
 
-            public abstract class WithoutResponse : ControllerBase, IServiceWithoutResponse<T1, T2>
+            public abstract class WithoutResponse : BaseController, IServiceWithoutResponse<T1, T2>
             {
                 public abstract Task ExecuteAsync(T1 param1, T2 param2, CancellationToken cancellationToken);
             }
@@ -34,12 +35,12 @@ namespace Codeed.Framework.Services
 
         public static class WithParameters<T1, T2, T3>
         {
-            public abstract class WithResponse<TRet> : ControllerBase, IService<T1, T2, T3, TRet>
+            public abstract class WithResponse<TRet> : BaseController, IService<T1, T2, T3, TRet>
             {
                 public abstract Task<TRet> ExecuteAsync(T1 param1, T2 param2, T3 param3, CancellationToken cancellationToken);
             }
 
-            public abstract class WithoutResponse : ControllerBase, IServiceWithoutResponse<T1, T2, T3>
+            public abstract class WithoutResponse : BaseController, IServiceWithoutResponse<T1, T2, T3>
             {
                 public abstract Task ExecuteAsync(T1 param1, T2 param2, T3 param3, CancellationToken cancellationToken);
             }
@@ -47,12 +48,12 @@ namespace Codeed.Framework.Services
 
         public static class WithoutParameters
         {
-            public abstract class WithResponse<TRet> : ControllerBase, IService<TRet>
+            public abstract class WithResponse<TRet> : BaseController, IService<TRet>
             {
                 public abstract Task<TRet> ExecuteAsync(CancellationToken cancellationToken);
             }
 
-            public abstract class WithoutResponse : ControllerBase, IService
+            public abstract class WithoutResponse : BaseController, IService
             {
                 public abstract Task ExecuteAsync(CancellationToken cancellationToken);
             }
