a29f7edb1adb43699c1c8aed9f13958d5bb4fb9b
Guilherme Gavazzoni <guilherme.gavazzoni@outlook.com>
2023-01-18 15:42:50 -0300

    create configureLogger function to allows serilog configuration

--- net/src/Codeed.Framework.AspNet/Serilog/SerilogConfigurationExtensions.cs
+++ net/src/Codeed.Framework.AspNet/Serilog/SerilogConfigurationExtensions.cs
@@ -4,22 +4,29 @@ using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
 using Serilog.Filters;
+using System;
 
 namespace Codeed.Framework.AspNet.Serilog
 {
     public static class SerilogConfigurationExtensions
     {
-        public static void AddSerilogApi(this IConfiguration configuration, string applicationName)
+        public static void AddSerilogApi(this IConfiguration configuration, string applicationName, Func<LoggerConfiguration, LoggerConfiguration> configureLogger = null)
         {
-            Log.Logger = new LoggerConfiguration()
+            var loggerConfiguration = new LoggerConfiguration()
             //.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information)
             .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Information)
             .Enrich.FromLogContext()
             .Enrich.WithCorrelationId()
             .Enrich.WithProperty("ApplicationName", $"API Serilog - {applicationName}")
             .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.StaticFiles"))
-            .WriteTo.Async(wt => wt.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Properties:j}{NewLine}{Exception}"))
-            .CreateLogger();
+            .WriteTo.Async(wt => wt.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Properties:j}{NewLine}{Exception}"));
+
+            if (configureLogger != null)
+            {
+                loggerConfiguration = configureLogger(loggerConfiguration);
+            }
+
+            Log.Logger = loggerConfiguration.CreateLogger();
         }
     }
 }
