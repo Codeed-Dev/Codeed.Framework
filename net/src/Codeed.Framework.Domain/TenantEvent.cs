b4b9edfbf8352fb3986bded031b0550b8e1b2f43
Guilherme Gavazzoni <guilherme.gavazzoni@outlook.com>
2023-02-14 07:57:14 -0300

    fix: added constructor without parameters on TenantEvent

--- net/src/Codeed.Framework.Domain/TenantEvent.cs
+++ net/src/Codeed.Framework.Domain/TenantEvent.cs
@@ -7,6 +7,10 @@ namespace Codeed.Framework.Domain
     {
         public string Tenant { get; set; }
 
+        public TenantEvent() : base()
+        {
+        }
+
         [JsonConstructor]
         protected TenantEvent(Guid id, DateTime timestamp, string tenant) : base(id, timestamp)
         {
