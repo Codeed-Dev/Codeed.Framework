c212f1fd5a879c67afe743e128b0ebca70a9c951
Guilherme Gavazzoni <guilherme.gavazzoni@outlook.com>
2023-09-07 10:11:29 -0300

    criado mediator events publisher

---
+++ net/src/Codeed.Framework.Data/MediatorDomainEventsPublisher.cs
@@ -0,0 +1,31 @@
+﻿using Codeed.Framework.Domain;
+using Codeed.Framework.Tenant;
+using MediatR;
+
+namespace Codeed.Framework.Data
+{
+    public class MediatorDomainEventsPublisher : IDomainEventsPublisher
+    {
+        private readonly IMediator _mediator;
+        private readonly ITenantService _tenantService;
+
+        public MediatorDomainEventsPublisher(IMediator mediator, ITenantService tenantService)
+        {
+            _mediator = mediator;
+            _tenantService = tenantService;
+        }
+
+        public async Task Publish(IEnumerable<Event> events)
+        {
+            foreach (var @event in events)
+            {
+                if (@event is ITenantEvent tenantEvent)
+                {
+                    tenantEvent.Tenant = _tenantService.Tenant;
+                }
+
+                await _mediator.Publish(@event);
+            }
+        }
+    }
+}
