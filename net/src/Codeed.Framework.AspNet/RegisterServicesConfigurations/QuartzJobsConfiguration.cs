8204e18648787e20a58c10006519571ad0b6b827
Guilherme Gavazzoni <guilherme.gavazzoni@outlook.com>
2024-02-24 20:42:02 -0300

    created send grid and quartz configuration

---
+++ net/src/Codeed.Framework.AspNet/RegisterServicesConfigurations/QuartzJobsConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Quartz.AspNetCore;
+using System.Reflection;
+
+namespace Codeed.Framework.AspNet.RegisterServicesConfigurations
+{
+    public static class RegisterCodeedFrameworkQuartzConfigurationExtensions
+    {
+        public static void ConfigureQuartz(this RegisterCodeedFrameworkOptions codeedOptions, Action<IServiceCollectionQuartzConfigurator>? configure = null)
+        {
+            var options = new QuartzJobsConfiguration(configure);
+            codeedOptions.AddServiceConfiguration(options);
+        }
+    }
+
+    public class QuartzJobsConfiguration : ICodeedServiceConfiguration
+    {
+        private readonly Action<IServiceCollectionQuartzConfigurator>? _configure;
+
+        public QuartzJobsConfiguration(Action<IServiceCollectionQuartzConfigurator>? configure = null)
+        {
+            _configure = configure;
+        }
+
+        public void RegisterServices(IServiceCollection services, IEnumerable<Assembly> assemblies)
+        {
+            services.AddQuartz(_configure);
+
+            services.AddQuartzServer(options =>
+            {
+                options.WaitForJobsToComplete = true;
+            });
+        }
+    }
+}
---
+++ net/src/Codeed.Framework.AspNet/RegisterServicesConfigurations/SendGridEmailServiceConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scaffolding.Core.Application.Environment;
+using System.Reflection;
+
+namespace Codeed.Framework.AspNet.RegisterServicesConfigurations
+{
+    public static class RegisterCodeedFrameworkSendGridConfigurationExtensions
+    {
+        public static void ConfigureSendGrid(this RegisterCodeedFrameworkOptions codeedOptions, IConfiguration configuration)
+        {
+            var options = new SendGridEmailServiceConfiguration(configuration);
+            codeedOptions.AddServiceConfiguration(options);
+        }
+    }
+
+    public class SendGridEmailServiceConfiguration : ICodeedServiceConfiguration
+    {
+        private readonly IConfiguration _configuration;
+
+        public SendGridEmailServiceConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void RegisterServices(IServiceCollection services, IEnumerable<Assembly> assemblies)
+        {
+            services.Configure<SendGridConfiguration>(_configuration.GetSection("SendGrid"));
+        }
+    }
+}
